0fec2fd82842f92ed270fad8c58827e22556f94c
martech <wrkmartech@gmail.com>
2023-12-19 03:09:58 +0200

    add solution for 03-streams/02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,32 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.buffer = '';
   }
 
   _transform(chunk, encoding, callback) {
+    chunk = chunk.toString();
+    if (chunk.indexOf(os.EOL) !== -1) {
+      const parts = chunk.split(os.EOL);
+      parts.forEach((part) => {
+        if (part !== parts.at(-1)) {
+          this.push(this.buffer + part);
+          this.buffer = '';
+        } else {
+          this.buffer = part;
+        }
+      });
+    } else {
+      this.buffer += chunk;
+    }
+    callback();
   }
 
   _flush(callback) {
+    if (this.buffer) {
+      this.push(this.buffer);
+    }
+    callback();
   }
 }
 
