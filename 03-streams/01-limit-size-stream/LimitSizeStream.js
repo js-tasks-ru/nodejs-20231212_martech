002eb2d5e35dbf3037c9dc1a261b33dc938cd6a7
martech <wrkmartech@gmail.com>
2023-12-19 03:00:25 +0200

    add solution for 03-streams/01-limit-size-stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,22 @@
-const stream = require('stream');
+const { Transform } = require('node:stream');
+const { Buffer } = require('node:buffer');
 const LimitExceededError = require('./LimitExceededError');
 
-class LimitSizeStream extends stream.Transform {
-  constructor(options) {
+class LimitSizeStream extends Transform {
+  constructor(options = {}) {
     super(options);
+
+    this.limit = options.limit;
+    this.bufferBytes = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.bufferBytes += Buffer.byteLength(chunk);
+    if (this.bufferBytes <= this.limit) {
+      callback(null, chunk);
+    } else {
+      callback(new LimitExceededError(), chunk);
+    }
   }
 }
 
--- 03-streams/01-limit-size-stream/index.js
+++ 03-streams/01-limit-size-stream/index.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const limitedStream = new LimitSizeStream({limit: 8, encoding: 'utf-8'}); // 8 байт
 const outStream = fs.createWriteStream('out.txt');
 
+limitedStream.on('error', (err) => {
+  console.log(`Error code: %s, error message: '%s'`, err.code, err.message);
+});
+
 limitedStream.pipe(outStream);
 
 limitedStream.write('hello'); // 'hello' - это 5 байт, поэтому эта строчка целиком записана в файл
